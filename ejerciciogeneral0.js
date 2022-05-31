618d8e0b41ef146d90b72a745e6b507d7f69f3d1
Federico Costa <federicojoaquincosta@gmail.com>
2022-05-31 10:00:22 -0300

    v1.2

--- ejerciciogeneral0.js
+++ ejerciciogeneral0.js
@@ -43,22 +43,9 @@ function comprar() {
     precioFinal = precioTotalDonSatur + precioTotalOreos + precioTotalFrutigran;
     formaPago = document.querySelector("#opcion_pago").value;
 
-    if (productosTotales < 7 && formaPago == 6) {
-        document.querySelector("#info_compra").innerHTML = `
-        <h3>Deberá comprar 7 productos o más para pagar en 6 cuotas</h3>
-        `
-    } else  if (productosTotales < 3 && formaPago >= 3) {
-        document.querySelector("#info_compra").innerHTML = `
-        <h3>Deberá comprar 3 productos o más para pagar en 3 cuotas</h3>
-        `
-    }
-
-    if (formaPago == 0) {
+    if (formaPago == 0 && productosTotales > 0) {
         precioFinal = precioFinal - (precioFinal * 0.2);
-        
-    }
-    if (productosTotales > 0 ) {
-    document.querySelector("#info_compra").innerHTML = `
+        document.querySelector("#info_compra").innerHTML = `
     <h3>Cantidad de Oreos compradas: ${cantidadOreos}</h3>
     <br>
     <h3>Precio total Oreos: ${precioTotalOreos}</h3>
@@ -75,24 +62,83 @@ function comprar() {
     <br>
     <h3>Precio Final: ${precioFinal}</h3>
     <br>
-    <h3>(se aplica un 20% de descuento si paga en efectivo)</h3>
-    <br>
-    <h3>Número de cuotas: ${formaPago}</h3>
+    <h3>Felicidades! al pagar en efectivo tiene un 20% de descuento</h3>
     <br>
     <h3></h3>
-    <br>
-    <h3></h3>
-    <br>
-    <h3></h3>
-    <br>
-    <h3></h3>
-    <br>
-
     `
+    } else if (formaPago == 1 && productosTotales > 0) {
+        document.querySelector("#info_compra").innerHTML = `
+        <h3>Cantidad de Oreos compradas: ${cantidadOreos}</h3>
+        <br>
+        <h3>Precio total Oreos: ${precioTotalOreos}</h3>
+        <br>
+        <h3>Cantidad De Don Satur compradas: ${cantidadDonSatur}</h3>
+        <br>
+        <h3>Precio total Don Satur: ${precioTotalDonSatur}</h3>
+        <br>
+        <h3>Cantidad de Frutigran compradas: ${cantidadFrutigran}</h3>
+        <br>
+        <h3>Precio total Frutigran: ${precioTotalFrutigran}</h3>
+        <br>
+        <h3>Cantidad de productos totales: ${productosTotales}</h3>
+        <br>
+        <h3>Precio Final: ${precioFinal}</h3>
+        <br>
+        <h3>Número de cuotas: ${formaPago}</h3>
+        <br>
+        <h3></h3>
+        `
+    } else if (formaPago == 3 && productosTotales < 3) {
+        document.querySelector("#info_compra").innerHTML = `
+        <h3>Deberá comprar 3 productos o más para pagar en 3 cuotas</h3>
+        `
+    } else if (formaPago == 3 && productosTotales >= 3) {
+        document.querySelector("#info_compra").innerHTML = `
+        <h3>Cantidad de Oreos compradas: ${cantidadOreos}</h3>
+        <br>
+        <h3>Precio total Oreos: ${precioTotalOreos}</h3>
+        <br>
+        <h3>Cantidad De Don Satur compradas: ${cantidadDonSatur}</h3>
+        <br>
+        <h3>Precio total Don Satur: ${precioTotalDonSatur}</h3>
+        <br>
+        <h3>Cantidad de Frutigran compradas: ${cantidadFrutigran}</h3>
+        <br>
+        <h3>Precio total Frutigran: ${precioTotalFrutigran}</h3>
+        <br>
+        <h3>Cantidad de productos totales: ${productosTotales}</h3>
+        <br>
+        <h3>Precio Final: ${precioFinal}</h3>
+        <br>
+        <h3>Número de cuotas: ${formaPago}</h3>
+        <br>
+        <h3></h3>
+        `
+    } else if (formaPago == 6 && productosTotales < 7) {
+        document.querySelector("#info_compra").innerHTML = `
+        <h3>Deberá comprar 7 productos o más para pagar en 6 cuotas</h3>
+        `
+    } else if (formaPago == 6 && productosTotales >= 7) {
+        document.querySelector("#info_compra").innerHTML = `
+        <h3>Cantidad de Oreos compradas: ${cantidadOreos}</h3>
+        <br>
+        <h3>Precio total Oreos: ${precioTotalOreos}</h3>
+        <br>
+        <h3>Cantidad De Don Satur compradas: ${cantidadDonSatur}</h3>
+        <br>
+        <h3>Precio total Don Satur: ${precioTotalDonSatur}</h3>
+        <br>
+        <h3>Cantidad de Frutigran compradas: ${cantidadFrutigran}</h3>
+        <br>
+        <h3>Precio total Frutigran: ${precioTotalFrutigran}</h3>
+        <br>
+        <h3>Cantidad de productos totales: ${productosTotales}</h3>
+        <br>
+        <h3>Precio Final: ${precioFinal}</h3>
+        <br>
+        <h3>Número de cuotas: ${formaPago}</h3>
+        <br>
+        <h3></h3>
+        `
+    }
 }
-
-}
-
-
-
-    
